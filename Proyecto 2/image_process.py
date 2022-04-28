7376a284df3d9693878bdd6220ef1478d59fbe76
Ivette612 <you@example.com>
2022-04-28 00:17:55 -0500

    image process como método

--- Proyecto 2/image_process.py
+++ Proyecto 2/image_process.py
@@ -1,16 +1,28 @@
 from PIL import Image
 import io
 
-try:
-    photo= '4.png'
-except:
-    print('Skipping unreadable image foo.png')
 
+def read_file(data):
+    """Process file
 
-#open the image
-img = Image.open(photo, mode='r') 
-# resize the image
-img = img.resize((300,300))
-byteIO = io.BytesIO()
-img.save(byteIO, format='PNG')
-byteArr = byteIO.getvalue()
+    Parameters
+    ----------
+    file_png : data
+
+    Returns
+    -------
+    byte of arrays
+    """
+
+    try:
+        im = Image.open('docs/4.png')
+
+    except:
+        print('Skipping unreadable image foo.png')
+
+    # resize the image
+    img = im.resize((300, 300)) 
+    byteIO = io.BytesIO()
+    img.save(byteIO, format='PNG')
+    byteArr = byteIO.getvalue()
+    return byteArr
